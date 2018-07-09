6e11ba8f835b6061948d7b9b4d1f92cbc6a0ac6c
yomZsamora <samora.y@adzumi.co.ke>
2018-07-09 14:07:58 +0300

    Start of HomeActivity Setup.

--- app/src/main/java/com/adzumi/capstone/ui/HomeActivity.java
+++ app/src/main/java/com/adzumi/capstone/ui/HomeActivity.java
@@ -18,35 +18,4 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-        searchView.setQueryHint("Search Keyword ...");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Log.e("onQueryTextChange", "called");
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
-                intent.putExtra("query", query);
-                startActivity(intent);
-                return false;
-            }
-        });
-        return true;
-    }
 }
