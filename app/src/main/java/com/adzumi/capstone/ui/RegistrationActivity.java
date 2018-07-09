d9188576003685f59c912763e5a02a9f35cf97f0
yomZsamora <samora.y@adzumi.co.ke>
2018-07-09 11:45:08 +0300

    Created Packages.

--- app/src/main/java/com/adzumi/capstone/ui/RegistrationActivity.java
+++ app/src/main/java/com/adzumi/capstone/ui/RegistrationActivity.java
@@ -1,11 +1,12 @@
-package com.adzumi.capstone;
+package com.adzumi.capstone.ui;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import com.adzumi.capstone.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
