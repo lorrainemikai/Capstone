d34fc1ffb6345acb6af1f434afe99a61a3d240fd
yomZsamora <samora.y@adzumi.co.ke>
2018-07-09 21:48:46 +0300

    google-services.json

--- app/src/main/java/com/adzumi/capstone/ui/RegistrationActivity.java
+++ app/src/main/java/com/adzumi/capstone/ui/RegistrationActivity.java
@@ -1,19 +1,27 @@
 package com.adzumi.capstone.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adzumi.capstone.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = RegistrationActivity.class.getSimpleName();
 
     @BindView(R.id.loginTextView) TextView mLoginTextView;
     @BindView(R.id.nameEditText) EditText mNameEditText;
@@ -22,13 +30,18 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @BindView(R.id.loginButton) Button mLoginButton;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mLoginTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
     }
 
     @Override
@@ -38,5 +51,28 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
             startActivity(intent);
             finish();
         }
+        if (view == mLoginButton) {
+            createNewUser();
+        }
+    }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
