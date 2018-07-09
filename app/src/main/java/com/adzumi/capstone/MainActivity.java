cd58fcb0da69e0aacacac5a3c0260226c419296c
yomZsamora <samora.y@adzumi.co.ke>
2018-07-09 11:13:00 +0300

    RegistrationActivity form complete.

--- app/src/main/java/com/adzumi/capstone/MainActivity.java
+++ app/src/main/java/com/adzumi/capstone/MainActivity.java
@@ -1,13 +1,33 @@
 package com.adzumi.capstone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == mCreateUserButton){
+            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
