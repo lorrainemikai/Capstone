47c705cd73511a8340f0563770607a852ae6bb51
yomZsamora <samora.y@adzumi.co.ke>
2018-07-09 11:24:02 +0300

    Finalized RegistrationActivity.

--- app/src/main/java/com/adzumi/capstone/RegistrationActivity.java
+++ app/src/main/java/com/adzumi/capstone/RegistrationActivity.java
@@ -1,13 +1,34 @@
 package com.adzumi.capstone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegistrationActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == mLoginTextView){
+            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
