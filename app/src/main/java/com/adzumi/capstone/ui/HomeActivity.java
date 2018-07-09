a1999b8740464643e1ff3aedf355035a04804690
yomZsamora <samora.y@adzumi.co.ke>
2018-07-09 12:03:51 +0300

    Modified Primary Colors.

--- app/src/main/java/com/adzumi/capstone/ui/HomeActivity.java
+++ app/src/main/java/com/adzumi/capstone/ui/HomeActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.adzumi.capstone.R;
+
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
